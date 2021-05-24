6f0e1ce4be755ef5cfbc10f914b31e46bc7ae2f3
Randy Butts <rbutts@chq.org>
2021-05-24 12:04:43 -0400

    Pushing unprocessed script to troubleshoot

--- ChautauquaBundleFix.js
+++ ChautauquaBundleFix.js
@@ -1,1 +1,139 @@
-"use strict";function _slicedToArray(e,r){return _arrayWithHoles(e)||_iterableToArrayLimit(e,r)||_unsupportedIterableToArray(e,r)||_nonIterableRest()}function _nonIterableRest(){throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}function _unsupportedIterableToArray(e,r){if(e){if("string"==typeof e)return _arrayLikeToArray(e,r);var t=Object.prototype.toString.call(e).slice(8,-1);return"Object"===t&&e.constructor&&(t=e.constructor.name),"Map"===t||"Set"===t?Array.from(e):"Arguments"===t||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t)?_arrayLikeToArray(e,r):void 0}}function _arrayLikeToArray(e,r){(null==r||r>e.length)&&(r=e.length);for(var t=0,n=new Array(r);t<r;t++)n[t]=e[t];return n}function _iterableToArrayLimit(e,r){var t=e&&("undefined"!=typeof Symbol&&e[Symbol.iterator]||e["@@iterator"]);if(null!=t){var n,o,a=[],s=!0,u=!1;try{for(t=t.call(e);!(s=(n=t.next()).done)&&(a.push(n.value),!r||a.length!==r);s=!0);}catch(e){u=!0,o=e}finally{try{s||null==t.return||t.return()}finally{if(u)throw o}}return a}}function _arrayWithHoles(e){if(Array.isArray(e))return e}var _require=require("core-js/fn/symbol"),replace=_require.replace,PageName=window.location.pathname.replace("/Online/","");switch(PageName){case"bundleSelect.asp":BundleSelectEntryPoint();break;case"bundleResult.asp":BundleResultEntryPoint();break;case"orderQuestions.asp":OrderQuestionsEntryPoint()}function convWordToNum(e){switch(e.toLowerCase()){case"one":return 1;case"two":return 2;case"three":return 3;case"four":return 4;case"five":return 5;case"six":return 6;case"seven":return 7;case"eight":return 8;case"nine":return 9}}function SaveBundleInfo(){var e=document.querySelectorAll(".bundle-element-container")[1].querySelectorAll("input[type=checkbox]"),r=[];e.forEach((function(e){if(1==e.checked){var t=convWordToNum(document.querySelector('label[for="'.concat(e.value,'"] > span.bundle-performance-description')).innerText.replace("Grounds Access Pass Week ",""));r.push(t)}})),r.sort();var t={name:document.querySelector("#bundle-item-title").innerText,weeks:r};sessionStorage.setItem("unprocessedBundle",JSON.stringify(t))}function BundleSelectEntryPoint(){document.querySelector("#bundle-item-title").innerText.includes("Grounds Access Pass")&&(null!==document.querySelector("#utility-menu-item-logout")?(document.querySelector("form[name=bundlesForm]").addEventListener("submit",SaveBundleInfo),document.querySelectorAll(".bundle-element-container")[1].querySelectorAll("input[type=checkbox]").forEach((function(e){e.addEventListener("change",SaveBundleInfo)}))):window.location="/Online/login.asp?targetPage=bundleSelect.asp")}function BundleResultEntryPoint(){var e=JSON.parse(sessionStorage.getItem("unprocessedBundle"));null!==e&&fetch("/Online/orderQuestions.asp").then((function(e){return e.text()})).then((function(r){var t=(new DOMParser).parseFromString(r,"text/html").documentElement.querySelectorAll('input[type=text][name^="BOset::WSorder::Bundle"]'),n=Array.from(t).map((function(e){return e.name.match(/Bundles::([A-z0-9\-]+)/)[1]})),o=JSON.parse(sessionStorage.getItem("processedBundles")||"{}"),a=Object.keys(o);n.filter((function(e){return!a.includes(e)})).forEach((function(r){o[r]=e})),sessionStorage.setItem("processedBundles",JSON.stringify(o))}))}function replaceOrderQuestions(){for(var e=JSON.parse(sessionStorage.getItem("processedBundles")||"{}"),r=0,t=Object.entries(e);r<t.length;r++){var n=_slicedToArray(t[r],2),o=n[0],a=n[1],s=document.querySelector('input[type=hidden][name^="BOset::WSorder::Bundles::'.concat(o,'"]'));null!==s&&(s.closest(".admission-section").firstElementChild.innerHTML="".concat(a.name,'<br/><small class="unstyled">Weeks ').concat(a.weeks.join(", "),"</small>"))}}function OrderQuestionsEntryPoint(){document.addEventListener("DOMContentLoaded",replaceOrderQuestions)}
+const { replace } = require("core-js/fn/symbol");
+
+let PageName = window.location.pathname.replace('/Online/', '');
+
+switch (PageName) {
+    case "bundleSelect.asp":
+        BundleSelectEntryPoint();
+        break;
+    case "bundleResult.asp":
+        BundleResultEntryPoint();
+        break;
+    case "orderQuestions.asp":
+        OrderQuestionsEntryPoint();
+        break;
+}
+
+function convWordToNum(word) {
+    // Yeah this is dumb but it's late
+    switch (word.toLowerCase()) {
+        case "one":
+            return 1;
+        case "two":
+            return 2;
+        case "three":
+            return 3;
+        case "four":
+            return 4;
+        case "five":
+            return 5;
+        case "six":
+            return 6;
+        case "seven":
+            return 7;
+        case "eight":
+            return 8;
+        case "nine":
+            return 9;
+
+        default:
+            break;
+    }
+}
+
+
+function SaveBundleInfo() {
+    // Grab checkboxes and pull selected
+    let ChkBoxes = document.querySelectorAll(".bundle-element-container")[1]
+        .querySelectorAll("input[type=checkbox]");
+
+    let weeks = []
+    ChkBoxes.forEach((box) => {
+        if (box.checked == true) {
+            let weekLbl = document.querySelector(`label[for="${box.value}"] > span.bundle-performance-description`)
+                .innerText.replace("Grounds Access Pass Week ", "");
+            let weekNum = convWordToNum(weekLbl);
+            weeks.push(weekNum);
+        }
+    });
+    weeks.sort();
+
+    // Create Bundle Object
+    let bundle = {
+        'name': document.querySelector("#bundle-item-title").innerText,
+        'weeks': weeks
+    };
+    // Save
+    sessionStorage.setItem('unprocessedBundle', JSON.stringify(bundle));
+}
+
+function BundleSelectEntryPoint() {
+    // Check DOM if valid bundle for code injection
+    let BundleTitle = document.querySelector('#bundle-item-title').innerText;
+    if (!BundleTitle.includes("Grounds Access Pass")) {
+        return;
+    }
+    // Check if logged in, redirect if not
+    if (document.querySelector("#utility-menu-item-logout") === null) {
+        window.location = "/Online/login.asp?targetPage=bundleSelect.asp";
+        return;
+    }
+
+    // Code injection starts here
+    document.querySelector("form[name=bundlesForm]")
+        .addEventListener("submit", SaveBundleInfo);
+    document.querySelectorAll(".bundle-element-container")[1]
+        .querySelectorAll("input[type=checkbox]").forEach(box => {
+            box.addEventListener("change", SaveBundleInfo);
+        })
+}
+
+function BundleResultEntryPoint() {
+    // Check for an unprocessed bundle. Exit if not found.
+    const bundle = JSON.parse(sessionStorage.getItem('unprocessedBundle'));
+    if (bundle === null) {
+        return;
+    }
+
+    // Grab page
+    fetch('/Online/orderQuestions.asp')
+        .then(response => response.text())
+        .then(data => {
+            // Scrape orderQuestions ahead of time, and pull a list of bundles
+            let parser = new DOMParser();
+            const qDoc = parser.parseFromString(data, "text/html");
+            const elms = qDoc.documentElement.querySelectorAll('input[type=text][name^="BOset::WSorder::Bundle"]');
+            const qBundleIds = Array.from(elms).map(elm => {
+                return elm.name.match(/Bundles::([A-z0-9\-]+)/)[1];
+            });
+
+            // Compare to bundles in storage
+            let processedBundles = JSON.parse(sessionStorage.getItem('processedBundles') || '{}');
+            const pBundleIds = Object.keys(processedBundles);
+            let difference = qBundleIds.filter(x => !pBundleIds.includes(x));
+
+            difference.forEach(id => {
+                processedBundles[id] = bundle;
+            });
+
+            // Save back to storage
+            sessionStorage.setItem('processedBundles', JSON.stringify(processedBundles));
+        });
+}
+
+function replaceOrderQuestions(){
+    // Check for bundles
+    const processedBundles = JSON.parse(sessionStorage.getItem('processedBundles') || '{}');
+    for (const [id, bundle] of Object.entries(processedBundles)) {
+        let formsetInput = document.querySelector(`input[type=hidden][name^="BOset::WSorder::Bundles::${id}"]`)
+        if (formsetInput === null) {
+            continue;
+        }
+        formsetInput.closest(".admission-section").firstElementChild
+            .innerHTML = `${bundle.name}<br/><small class="unstyled">Weeks ${bundle.weeks.join(', ')}</small>`;
+    }
+}
+
+function OrderQuestionsEntryPoint() {
+    document.addEventListener('DOMContentLoaded', replaceOrderQuestions)
+}
